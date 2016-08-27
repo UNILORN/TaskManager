dda7bee6eabb4ce85b593ab93052f4fad300cf12
UNILORN <b5159@oic.jp>
2016-08-28 05:49:04 +0900

    new file

--- app.js
+++ app.js
@@ -1,39 +1,58 @@
 var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server)
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var ejs = require('ejs');
-var app = express();
 
-// view engine setup
+
 app.set('view engine', 'ejs');
 app.engine('ejs',ejs.renderFile);
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-app.use(express.static('public'));
-
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  database : 'granfrontend_work',
-  host     : 'localhost',
-  user     : 'root',
-  password : ''
-});
+app.use(express.static('/socket.io'));
+app.use(express.static(__dirname + '/public'));
 
 
-app.get('/',function(req,res) {
-  connection.query('SELECT * FROM `user`', function (error, results, fields) {
-    console.log(results);
-    res.render('index.ejs',{
-      title: 'Hello World!',
-      content: 'Hello World!',
-      sql: results
-    });
+
+function new_text(list){
+  socket.emit('new_text',list);
+
+
+}
+
+io.on('connection',function(socket){
+  socket.on('before_req',function(list){
+    new_text(list);
   });
-
-
+  socket.on('after_req',function(list){
+    new_text(list);
+  });
+  socket.on('answer_req',function(list){
+    new_text(list);
+  });
+  socket.on('problem_req',function(list){
+    new_text(list);
+  });
 });
+
+
+app.get('/public/stylesheets/style.css', function(req, res) {
+    res.sendfile(__dirname + '/public/stylesheets/style.css');
+});
+
+app.get('/common/plug/socket.io-1.4.5.js ', function(req, res) {
+    res.sendfile(__dirname + '/common/plug/socket.io-1.4.5.js ');
+});
+app.get('/public/javascripts/vue.js', function(req, res) {
+    res.sendfile(__dirname + '/public/javascripts/vue.js');
+});
+var index = require('./routes/index');
+app.use('/', index);
+
+var user = require('./routes/users');
+app.use('/user', user);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/vue.js
+++ public/javascripts/vue.js
@@ -1,6 +1,107 @@
-new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue.js!'
-  }
-})
+
+var socket = io();
+document.addEventListener("DOMContentLoaded", function() {
+
+    var content = new Vue({
+        el: '.task',
+        data: {
+            list: [{
+                id: 1,
+                task: '進捗と今後',
+                before_title: '今までやったこと',
+                beforetext: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                after_title: 'これからすること',
+                aftertext: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                problem_title: '問題',
+                problemtext: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                answer_title: '解決方法',
+                answertext: 'aaaaaaaaaaaaaaaaaaaaaaaaaa',
+                date:'○月△日第何週',//[]
+                username:[]
+            },{
+                id: 2,
+                task: '進捗と今後',
+                before_title: '今までやったこと',
+                beforetext: [],
+                after_title: 'これからすること',
+                aftertext: [],
+                problem_title: '問題',
+                problemtext: [],
+                answer_title: '解決方法',
+                answertext: [],
+                date:'○月△日第何週',
+                username:[]
+            },{
+                id: 3,
+                task: '進捗と今後',
+                before_title: '今までやったこと',
+                beforetext: [],
+                after_title: 'これからすること',
+                aftertext: [],
+                problem_title: '問題',
+                problemtext: [],
+                answer_title: '解決方法',
+                answertext: [],
+                date:'○月△日第何週',
+                username:[]
+            },{
+                id: 4,
+                task: '進捗と今後',
+                before_title: '今までやったこと',
+                beforetext: [],
+                after_title: 'これからすること',
+                aftertext: [],
+                problem_title: '問題',
+                problemtext: [],
+                answer_title: '解決方法',
+                answertext: [],
+                date:'○月△日第何週',
+                username:[]
+            }]
+        },
+        methods:{
+          problem_submit:function(index){
+            console.log(this.list[index].problem_input);
+            this.list[index].problem ='';
+            socket.emit('before_req',{before:this.list[index].before_input,id:index+1});
+          },
+          answer_submit:function(index){
+            console.log(this.list[index].answer_input);
+            this.list[index].problem ='';
+            socket.emit('before_req',{before:this.list[index].before_input,id:index+1});
+          },
+          before_submit:function(index){
+            console.log(this.list[index].before_input);
+            this.list[index].problem ='';
+            socket.emit('before_req',{before:this.list[index].before_input,id:index+1});
+          },
+          after_submit:function(index){
+            console.log(this.list[index].after_input);
+            this.list[index].problem ='';
+            socket.emit('before_req',{before:this.list[index].before_input,id:index+1});
+          }
+        }
+    });
+});
+
+socket.on('new_text',function(data){
+  content.list[data.id]. hensu .push(data);
+});
+
+//どうやって受け取ろう。。。
+(function() {
+    socket.on('old_text', function(data) {
+        if(data.beforetext != null){
+          content.list[data.id].beforetext.push(data.before);
+        }
+        if(data.aftertext != null){
+          content.list[data.id].aftertext.push(data.after);
+        }
+        if(data.problem != null){
+          content.list[data.id].problem.push(data.problem);
+        }
+        if(data.answetext != null){
+          content.list[data.id].answertext.push(data.answer);
+        }
+    });
+}());
